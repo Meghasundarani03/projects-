ea228ae0f760ce8be11f61e29745e1863e3d7e18
Megha Sundrani <134036208+Meghasundarani03@users.noreply.github.com>
2023-07-08 13:59:48 +0530

    Add files via upload

---
+++ AnalogWatch/script.js
@@ -0,0 +1,13 @@
+setInterval(() =>{
+    date = new Date();
+    hr = date.getHours();
+    min = date.getMinutes();
+    sec = date.getSeconds();
+    hrotation = 30*hr + min/2;
+    mrotation = 6*min;
+    srotation = 6*sec;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    secd.style.transform = `rotate(${srotation}deg)`;
+
+}, 1000);
